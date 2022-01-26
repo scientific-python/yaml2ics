cf800dab674952350b0998e0fde16720d6e3b191
Stefan van der Walt <stefanv@berkeley.edu>
2022-01-26 11:53:21 -0800

    Black and flake8 fixes

--- tests/test_calendar.py
+++ tests/test_calendar.py
@@ -2,38 +2,44 @@ import datetime
 import io
 import textwrap
 
-from util import parse_yaml
-
 from yaml2ics import events_to_calendar, files_to_calendar
 
 
 def test_calendar_structure():
     cal = events_to_calendar([])
     cal_str = cal.serialize()
-    assert cal_str.startswith('BEGIN:VCALENDAR')
-    assert cal_str.endswith('END:VCALENDAR')
+    assert cal_str.startswith("BEGIN:VCALENDAR")
+    assert cal_str.endswith("END:VCALENDAR")
+
 
 def test_calendar_event():
     cal = files_to_calendar(
-        [io.StringIO(textwrap.dedent(
-            '''
+        [
+            io.StringIO(
+                textwrap.dedent(
+                    """
             events:
               - summary: Earth Day
                 begin: 2021-04-22
                 url: https://earthday.org
                 location: Earth
-            '''
-        ))]
+            """
+                )
+            )
+        ]
     )
     cal_str = cal.serialize()
-    assert cal_str.startswith('BEGIN:VCALENDAR')
-    assert 'SUMMARY:Earth Day' in cal_str
-    assert cal_str.endswith('END:VCALENDAR')
+    assert cal_str.startswith("BEGIN:VCALENDAR")
+    assert "SUMMARY:Earth Day" in cal_str
+    assert cal_str.endswith("END:VCALENDAR")
+
 
 def test_calendar_default_timezone():
     cal = files_to_calendar(
-        [io.StringIO(textwrap.dedent(
-            '''
+        [
+            io.StringIO(
+                textwrap.dedent(
+                    """
             meta:
               tz: Europe/Helsinki
 
@@ -48,23 +54,25 @@ def test_calendar_default_timezone():
 
               - summary: Earth day (all day)
                 begin: 2022-04-22
-            '''
-        ))]
+            """
+                )
+            )
+        ]
     )
     cal_str = cal.serialize()
-    assert cal_str.startswith('BEGIN:VCALENDAR')
-    assert 'SUMMARY:New year' in cal_str
+    assert cal_str.startswith("BEGIN:VCALENDAR")
+    assert "SUMMARY:New year" in cal_str
     # It is possible that the ics-py TZID string changes, but hopefully this
     # substring is fairly safe to test against.
-    assert 'Europe/Helsinki:20220101T000000' in cal_str
+    assert "Europe/Helsinki:20220101T000000" in cal_str
     # Second event: explicit UTC offset specified.
     assert '"UTC+02:00":20220201T000000' in cal_str
-    assert cal_str.endswith('END:VCALENDAR')
+    assert cal_str.endswith("END:VCALENDAR")
 
     # Test again by normalizing to UTC.  Helsinki is two hours ahead, so the
     # times should be 22:00:00.
     cal.normalize(datetime.timezone.utc)
-    cal_norm_str = cal.serialize()
+    cal_norm_str = cal.serialize()  # noqa: F841
     # 1 Feb midnight
-    assert 'DTSTART:20211231T220000Z' # 1 jan
-    assert 'DTSTART:20220131T220000Z' # 1 feb
+    assert "DTSTART:20211231T220000Z"  # 1 jan
+    assert "DTSTART:20220131T220000Z"  # 1 feb
--- yaml2ics.py
+++ yaml2ics.py
@@ -1,28 +1,27 @@
-import yaml
-import sys
 import os
+import sys
 from datetime import datetime, tzinfo
 
-import ics
 import dateutil
 import dateutil.rrule
+import ics
+import yaml
 from dateutil.tz import gettz
 
-
 interval_type = {
-    'seconds': dateutil.rrule.SECONDLY,
-    'minutes': dateutil.rrule.MINUTELY,
-    'hours': dateutil.rrule.HOURLY,
-    'days': dateutil.rrule.DAILY,
-    'weeks': dateutil.rrule.WEEKLY,
-    'months': dateutil.rrule.MONTHLY,
-    'years': dateutil.rrule.YEARLY
+    "seconds": dateutil.rrule.SECONDLY,
+    "minutes": dateutil.rrule.MINUTELY,
+    "hours": dateutil.rrule.HOURLY,
+    "days": dateutil.rrule.DAILY,
+    "weeks": dateutil.rrule.WEEKLY,
+    "months": dateutil.rrule.MONTHLY,
+    "years": dateutil.rrule.YEARLY,
 }
 
 
-def event_from_yaml(event_yaml: dict, tz: tzinfo=None) -> ics.Event:
+def event_from_yaml(event_yaml: dict, tz: tzinfo = None) -> ics.Event:
     d = event_yaml
-    repeat = d.pop('repeat', None)
+    repeat = d.pop("repeat", None)
 
     # Strip all string values, since they often end on `\n`
     for key in d:
@@ -37,42 +36,50 @@ def event_from_yaml(event_yaml: dict, tz: tzinfo=None) -> ics.Event:
     event = ics.Event(**d)
 
     # Handle all-day events
-    if not ('duration' in d or 'end' in d):
+    if not ("duration" in d or "end" in d):
         event.make_all_day()
 
     if repeat:
-        interval = repeat['interval']
+        interval = repeat["interval"]
 
         if not len(interval) == 1:
-            print('Error: interval must specify seconds, minutes, hours, days, weeks, months, or years only', file=sys.stderr)
+            print(
+                "Error: interval must specify seconds, minutes, hours, days, weeks, months, or years only",
+                file=sys.stderr,
+            )
             sys.exit(-1)
 
         interval_measure = list(interval.keys())[0]
         if interval_measure not in interval_type:
-            print('Error: expected interval to be specified in seconds, minutes, hours, days, weeks, months, or years only', file=sys.stderr)
+            print(
+                "Error: expected interval to be specified in seconds, minutes, hours, days, weeks, months, or years only",
+                file=sys.stderr,
+            )
             sys.exit(-1)
 
-        if 'until' not in repeat:
-            print('Error: must specify end date for repeating events', file=sys.stderr)
+        if "until" not in repeat:
+            print("Error: must specify end date for repeating events", file=sys.stderr)
             sys.exit(-1)
 
         # This causes zero-length events, I guess overriding whatever duration might have been specified.
-        #event.end = d.get('end', None)
+        # event.end = d.get('end', None)
 
         rrule = dateutil.rrule.rrule(
             freq=interval_type[interval_measure],
-            until=repeat.get('until'),
-            dtstart=d['begin']
+            until=repeat.get("until"),
+            dtstart=d["begin"],
         )
 
-        rrule_lines = str(rrule).split('\n')
-        rrule_dtstart = [line for line in rrule_lines if line.startswith('DTSTART')][0]
-        rrule_dtstart = rrule_dtstart + 'Z'
-        rrule_rrule = [line for line in rrule_lines if line.startswith('RRULE')][0]
-        event.extra.append(ics.ContentLine(
-            name=rrule_rrule.split(':', 1)[0],
-            value=rrule_rrule.split(':', 1)[1],
-            ))
+        rrule_lines = str(rrule).split("\n")
+        rrule_dtstart = [line for line in rrule_lines if line.startswith("DTSTART")][0]
+        rrule_dtstart = rrule_dtstart + "Z"
+        rrule_rrule = [line for line in rrule_lines if line.startswith("RRULE")][0]
+        event.extra.append(
+            ics.ContentLine(
+                name=rrule_rrule.split(":", 1)[0],
+                value=rrule_rrule.split(":", 1)[1],
+            )
+        )
 
     event.dtstamp = datetime.utcnow().replace(tzinfo=dateutil.tz.UTC)
     if tz and event.floating and not event.all_day:
@@ -86,34 +93,34 @@ def events_to_calendar(events: list) -> str:
         cal.events.append(event)
     return cal
 
+
 def files_to_calendar(files: list) -> ics.Calendar:
     """'main' function: list of files to our result"""
-    all_events = [ ]
-    name = None
+    all_events = []
     for f in files:
-        if hasattr(f, 'read'):
+        if hasattr(f, "read"):
             calendar_yaml = yaml.load(f.read(), Loader=yaml.FullLoader)
         else:
-            calendar_yaml = yaml.load(open(f, 'r'), Loader=yaml.FullLoader)
+            calendar_yaml = yaml.load(open(f, "r"), Loader=yaml.FullLoader)
         tz = None
-        if 'meta' in calendar_yaml:
-            if 'tz' in calendar_yaml['meta']:
-                tz = gettz(calendar_yaml['meta']['tz'])
-        for event in calendar_yaml['events']:
+        if "meta" in calendar_yaml:
+            if "tz" in calendar_yaml["meta"]:
+                tz = gettz(calendar_yaml["meta"]["tz"])
+        for event in calendar_yaml["events"]:
             all_events.append(event_from_yaml(event, tz=tz))
     calendar = events_to_calendar(all_events)
     return calendar
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print('Usage: yaml2ics.py FILE1.yaml FILE2.yaml ...')
+        print("Usage: yaml2ics.py FILE1.yaml FILE2.yaml ...")
         sys.exit(-1)
 
     files = sys.argv[1:]
     for f in files:
         if not os.path.isfile(f):
-            print(f'Error: {f} is not a file', file=sys.stderr)
+            print(f"Error: {f} is not a file", file=sys.stderr)
             sys.exit(-1)
 
     calendar = files_to_calendar(files)
